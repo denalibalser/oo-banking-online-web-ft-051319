487843f3d0746a19e9d17bd116f0171c417f8c85
Denali Balser <denali.balser167@gmail.com>
2019-05-28 15:47:12 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,16 @@
 class BankAccount
-  attr_accessor 
+  attr_accessor
   attr_reader :name, :balance, :status
 
   def initialize(name)
     @name = name
     @balance = 1000
     @status = 'open'
+  end
+
+  def deposit(money)
+    @balance 
+  end
 
 
 end
