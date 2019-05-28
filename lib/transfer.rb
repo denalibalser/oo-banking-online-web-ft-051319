7e9b302164e4887bf8436435193db54c11f67139
Denali Balser <denali.balser167@gmail.com>
2019-05-28 16:01:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
-  
+  attr_accessor :sender, :receiver, :status,
+
+  def initialize
+    @sender = sender
+    @receiver = receiver
+    @status = 'pending'
+    @transfer_amount
+  end
+
 end
